37cb29b47815db4df6ac60c371d957a93657a894
Killian Habasque <killian.habasque@gmail.com>
2025-02-01 15:45:07 +0100

    style navbar & header

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,20 @@ import { NavbarDemo } from "./components/demo2";
 export default function Home() {
   return (
     <div id="root">
+      
+      <div className='bg-noise'></div>
+
       <NavbarDemo />
-      <div className="p-4 bg-white h-[80vh]">
-        <div className="bg-dark relative flex h-full w-full flex-col justify-center overflow-hidden rounded-2xl">
+      <div className="p-2 bg-white h-[100vh] pt-20">
+        <div className="bg-dark relative flex h-full w-full flex-col justify-center overflow-hidden rounded-2xl pt-[50px]">
           <RetroGrid />
           <div className='bg-test'></div>
-          <div className='bg-noise'></div>
+
           <Model />
           <WelcomeExample />
         </div>
       </div>
+
     </div>
   );
 }
